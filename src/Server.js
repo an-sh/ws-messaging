c3c5d0ee8a05d8cad97ad882085723f61ae0c218
an-sh <an-sh@users.noreply.github.com>
2016-09-27 17:44:43 +0300

    refactor: use flow comments, simplify tooling

--- src/Server.js
+++ src/Server.js
@@ -1,6 +1,4 @@
-// -*-Javascript-*-
 // @flow
-
 'use strict'
 
 const Client = require('./Client')
@@ -29,7 +27,7 @@ const defaults = {
  * @return {Promise<Object|undefined>|Object|undefined} Auth reply
  * data.
  */
-type ConnectionHook = (client: Client, data?: any) => Promise<any>
+/* :: type ConnectionHook = (client: Client, data?: any) => Promise<any> */
 
 /**
  * @typedef {Object} Server.ServerOptions
@@ -41,12 +39,14 @@ type ConnectionHook = (client: Client, data?: any) => Promise<any>
  * @property {number} [authTimeout=20000] Auth message wait timeout in
  * ms.
  */
+/* ::
 type ServerOptions = { Server?: constructor,
                        connectionHook?: ConnectionHook,
                        authTimeout?: number
                      }
+*/
 
-type SocketOptions = Object
+/* :: type SocketOptions = Object */
 
 /**
  * @extends EventEmitter
@@ -55,12 +55,14 @@ type SocketOptions = Object
  * @emits Server#error
  */
 class Server extends EventEmitter {
+  /* ::
   authTimeout: number
   Server: constructor
   clients: Map<string, Object>
   connectionHook: ConnectionHook
   socketOptions: Object
   wss: Object
+   */
 
   /**
    * Starts a server.
@@ -69,9 +71,9 @@ class Server extends EventEmitter {
    * @param {Server.ServerOptions} [serverOptions] Server options.
    * @param {Client.SocketOptions} [socketOptions] Socket options.
    */
-  constructor (wssOptions: Object,
-               serverOptions: ServerOptions = {},
-               socketOptions: SocketOptions = {}) {
+  constructor (wssOptions /* : Object */,
+               serverOptions /* : ServerOptions */ = {},
+               socketOptions /* : SocketOptions  */ = {}) {
     super()
     this.socketOptions = { WebSocket }
     assign(this.socketOptions, socketOptions)
@@ -80,7 +82,7 @@ class Server extends EventEmitter {
     this._setEvents(wssOptions)
   }
 
-  _setEvents (wssOptions: Object) : void {
+  _setEvents (wssOptions /* : Object */) /* : void */ {
     /**
      * Emits ready event.
      * @event Server#ready
@@ -95,13 +97,15 @@ class Server extends EventEmitter {
     this.wss.on('connection', socket => this._onConnection(socket))
   }
 
-  _onConnection (socket: Object & EventEmitter) : void {
+  _onConnection (socket /* : Object & EventEmitter */) /* : void */ {
     let timer =
         setTimeout(socket.close.bind(socket, CLOSE_FORBIDDEN), this.authTimeout)
     socket.once('message', data => this._addClient(socket, data, timer))
   }
 
-  _addClient (socket: EventEmitter, data: any, timer: number) : void {
+  _addClient (socket /* : EventEmitter */,
+              data /* : any */,
+              timer /* : number */) /* : void */ {
     let client
     clearTimeout(timer)
     uid(18).then(id => {
@@ -127,7 +131,7 @@ class Server extends EventEmitter {
     })
   }
 
-  _removeClient (id: string) : void {
+  _removeClient (id /* : string */) /* : void */ {
     this.clients.delete(id)
   }
 
@@ -136,7 +140,7 @@ class Server extends EventEmitter {
    * @param {string} id Client id.
    * @returns {Client|undefined} Client if found.
    */
-  getClient (id: string) : Client {
+  getClient (id /* : string */) /* : Client */ {
     return this.clients.get(id)
   }
 
@@ -145,7 +149,7 @@ class Server extends EventEmitter {
    * @param {code} [code=1000] Code as per WebSocket spec.
    * @returns {Promise<undefined>} Promise.
    */
-  close (code: number = 1000) : Promise<void> {
+  close (code /* : number */ = 1000) /* : Promise<void> */ {
     for (let [, client] of this.clients) {
       client.close(code)
     }
